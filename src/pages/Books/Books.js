5fbe241dd55926e26cc5f4f2f2dd45870cb84dd7
nmso2 <nmsubho@gmail.com>
2021-11-23 08:00:49 +0600

    first commit

---
+++ src/pages/Books/Books.js
@@ -0,0 +1,67 @@
+import { Paper } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+const Books = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                    m: 1,
+                    width: '45%',
+                    height: 500,
+                },
+            }}
+        >
+            <Paper
+                data-aos="fade-up-right"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center" elevation={3} />
+            <Paper
+                data-aos="fade-up-right"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center" elevation={3} />
+            <Paper
+                data-aos="fade-up-right"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center" elevation={3} />
+            <Paper
+                data-aos="fade-up-right"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center" elevation={3} />
+            <Paper
+                data-aos="fade-up"
+                data-aos-duration="3000" elevation={3} />
+            <Paper
+                data-aos="fade-up"
+                data-aos-duration="3000" elevation={3} />
+        </Box>
+    );
+};
+
+export default Books;
