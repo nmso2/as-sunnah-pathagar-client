8b3368eae4461a14ff2ff2ee36f8afbd83a862eb
nmso2 <nmsubho@gmail.com>
2021-11-27 17:20:01 +0600

    updated footer

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -12,22 +12,22 @@ const Footer = () => {
                         <div className="text-xs uppercase text-gray-400 font-medium mb-6">
                             About As-Sunnah Pathagar
                         </div>
-                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">He wears them. She wears them. Everyone wears them. Shop the top selling brands worn around the world at our low outlet pricing.</p>
+                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">ইসলামের সঠিক জ্ঞান অর্জনে মুসলিমদের সাহায্য করতেই আমাদের কার্যক্রম।</p>
                     </div>
 
                     <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12">
                         <div className="text-xs uppercase text-gray-400 font-medium mb-6">
-                            Products
+                            Books
                         </div>
 
                         <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                            Casual
+                            দুআ ও যিকির
                         </p>
                         <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                            Sunglass
+                            ইসলামী জ্ঞান চর্চা
                         </p>
                         <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                            Stylish Sunglass
+                            আত্মশুদ্ধি ও অনুপ্রেরণা
                         </p>
                     </div>
 
@@ -36,9 +36,9 @@ const Footer = () => {
                             Contact Us
                         </div>
 
-                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Mobile: +8801700000000</p>
-                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Email: info@sunglasscollection.com</p>
-                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Address: Dhaka, Bangladesh</p>
+                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Mobile: +8801623-371391</p>
+                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Email: assunnahpathagar@gmail.com</p>
+                        <p className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Address: Bheramara, Kushtia</p>
 
                     </div>
 
@@ -47,9 +47,9 @@ const Footer = () => {
                             Community
                         </div>
 
-                        <Link to='/' className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                            Discord
-                        </Link>
+                        <a href="https://www.facebook.com/%E0%A6%86%E0%A6%B8-%E0%A6%B8%E0%A7%81%E0%A6%A8%E0%A7%8D%E0%A6%A8%E0%A6%BE%E0%A6%B9-%E0%A6%AA%E0%A6%BE%E0%A6%A0%E0%A6%BE%E0%A6%97%E0%A6%BE%E0%A6%B0-107120294460789" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                            Facebook
+                        </a>
                         <Link to='/' className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
                             Twitter
                         </Link>
@@ -64,7 +64,8 @@ const Footer = () => {
             border-t border-gray-500 text-gray-400 text-sm 
             flex-col md:flex-row max-w-6xl">
                         <div className="mt-2">
-                            © Copyright 2019-2021. All Rights Reserved by <a className="uppercase" href="https://github.com/nmso2">Md. Nagib Mahfuz Subho.</a>
+                            © Copyright 2019-2021. All Rights Reserved by <a className="uppercase" href="https://www.facebook.com/%E0%A6%86%E0%A6%B8-%E0%A6%B8%E0%A7%81%E0%A6%A8%E0%A7%8D%E0%A6%A8%E0%A6%BE%E0%A6%B9-%E0%A6%AA%E0%A6%BE%E0%A6%A0%E0%A6%BE%E0%A6%97%E0%A6%BE%E0%A6%B0-107120294460789">As-Sunnah Pathagar</a><br />
+                            Design and Developed by <a className="uppercase" href="https://github.com/nmso2">Md. Nagib Mahfuz Subho</a>
                         </div>
                     </div>
                 </div>
