0f63eac0755bc69c8ef9754e7919e57677deea6e
nmso2 <nmsubho@gmail.com>
2021-11-27 22:14:45 +0600

    responsive

--- src/pages/Dashboard/ManageRequests/ManageRequest/ManageRequest.js
+++ src/pages/Dashboard/ManageRequests/ManageRequest/ManageRequest.js
@@ -40,10 +40,10 @@ const ManageRequest = (props) => {
                 <div className="lg:flex items-baseline justify-end">
                     <p className="lg:text-3xl lg:px-5">{requested.name}</p>
 
-                    <button className="border hidden border-blue-500 text-blue-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-blue-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleUpdateRequest('Out of Stock')}>Out of Stock</button>
-                    <button className="border hidden border-blue-500 text-blue-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-blue-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleUpdateRequest('Confirmed')}>Confirm</button>
+                    <button className="border border-blue-500 text-blue-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-blue-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleUpdateRequest('Out of Stock')}>Out of Stock</button>
+                    <button className="border border-blue-500 text-blue-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-blue-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleUpdateRequest('Confirmed')}>Confirm</button>
 
-                    <button className="border hidden border-red-500 text-red-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-red-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleCancelRequests(request._id)}>Calcel Request</button>
+                    <button className="border border-red-500 text-red-500 rounded-md px-5 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-red-600 focus:outline-none focus:shadow-outline lg:block" onClick={() => handleCancelRequests(request._id)}>Calcel Request</button>
                 </div>
                 <Accordion allowZeroExpanded className="py-2">
                     <AccordionItem>
